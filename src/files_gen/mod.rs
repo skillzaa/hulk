26d137bbd04de4789875cec69b2aec23c66638df
skilzaa <skillzaa.com@gmail.com>
2021-11-30 13:08:56 +0500

    finally some hotchpotch file with index

--- src/files_gen/mod.rs
+++ src/files_gen/mod.rs
@@ -1,30 +1,55 @@
 use crate::assets::{get_default_footer,get_default_header,get_default_nav,get_dark_css};
 use comrak::{markdown_to_html, ComrakOptions};
 use std::fs::{DirEntry};
+use bro::navbar::NavBar;
 use std::path::Path;
 use brown as bro;
 use std::io::{Error};
 use std::fs;
+// use nav
 
 #[derive(Debug)]
 pub struct FilesGen{
-  file_extention:&'static str, 
   url:&'static str, 
+  file:DirEntry,
 }
 impl FilesGen{
-  pub fn new(url:&'static str)->Self{
-      FilesGen {file_extention:"md",
-                url,  
-              }
+  pub fn new(file:DirEntry, url:&'static str)->Self{
+    FilesGen {file,url,}
   }
-  pub fn run(dir_entry:&DirEntry)->Result<bool,Error> {
+  pub fn run(&self,dir_entry:&DirEntry)->Result<bool,Error> {
     let file_name = bro::get_file_name(&dir_entry)?;
     let content = 
-    get_page(&dir_entry)?;
+    self.get_page()?;
     write_file(&file_name, &content)?;
     Ok(true)
   }
-  pub fn show(&self)->&'static str{"fff ggggg"}
+  fn get_nav(&self)->Result<String,Error>{
+    let n = 
+    NavBar::new(self.url);
+    match n {
+    Ok(item)=>{
+    Ok(item.gen_navbar())
+    },
+    Err(_e)=>{Err(_e)},
+    }
+  }
+  fn get_page(&self)->Result<String,Error>{
+    //--Build the Page..
+    let mut page = String::new();
+    page.push_str(get_default_header());
+    let n = self.get_nav();
+      match n {
+      Ok(item)=>{
+        page.push_str(&item);
+      },
+      Err(_e)=>{},
+      }
+    let md_to_html = md_to_html(&self.file)?;
+    page.push_str(md_to_html.as_str());
+    page.push_str(get_default_footer());
+    Ok(page)
+    }
 }
 
 //-------------------------------
@@ -36,16 +61,7 @@ let write_path = Path::new(&write_path_str);
 fs::write(write_path, content)?;
 Ok(true)
 }
-fn get_page(dir_entry:&DirEntry)->Result<String,Error>{
-//--Build the Page..
-let mut page = String::new();
-page.push_str(get_default_header());
-page.push_str(get_default_nav());
-let md_to_html = md_to_html(dir_entry)?;
-page.push_str(md_to_html.as_str());
-page.push_str(get_default_footer());
-Ok(page)
-}
+//................................
 fn md_to_html(dir_entry:&DirEntry)->Result<String,Error>{
 let md = fs::read_to_string(&dir_entry.path().as_path())?;
 let html = markdown_to_html(&md, &ComrakOptions::default());
@@ -60,15 +76,23 @@ println!("Dark Theme css generated..");
 
 }
 
+
 #[cfg(test)]
 mod tests{
 use super::*;
 
 #[test]
 fn first(){
-  let f = FilesGen::
-  new("delme/a/a1/a11/a111");
-  let s = f.show();
-  println!("{}",s);
+  let files = bro::get_files("data").unwrap();
+  let url = "data/snippets";
+  // let file = files.first().unwrap().to_owned();
+    for file in files {
+      let f = FilesGen::new(file,url);
+      let n = f.get_page().unwrap();
+      println!("===>{:?}",n);
+
+    }  
 }
-}
+
+
+}//mod
