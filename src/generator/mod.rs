adf0e32d8d748c294c207a2adfde450b560bac69
skilzaa <skillzaa.com@gmail.com>
2021-12-14 12:34:30 +0500

    moving forward

--- src/generator/mod.rs
+++ src/generator/mod.rs
@@ -11,8 +11,12 @@ impl Generator {
     }
     pub fn run()->Result<bool,Error>{
         //Step one delete old site folder
-        let old_site = remove_dir_brute("site")?;
-        
+        //PROBLEM THIS RETURN ERROR IF SIR NOT FOUND
+        //--CHECK IN BROWN
+        let _ = remove_dir_brute("site");
+        let dir_struct = 
+        clone_dir_structure("data", "site")?;
+        println!("{:?}",dir_struct);
         Ok(true)
     }
 
