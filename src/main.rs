eb4c0bb3b19ab4fc8dc086cbd8beb126d2fb5166
skilzaa <skillzaa.com@gmail.com>
2021-12-14 21:10:29 +0500

    started cleaning generator code, it will take many itwerations

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,20 @@ use brown::*;
 use brown::BrownError as Error;
 
 pub fn main() {
-  let clone = 
-  clone_dir_structure("data","site");
-  println!("{:?}",clone);
-  
-  //-->Step one delete old site folder
-  //remove_dir_brute("site").unwrap();
-  let mut dir_struct = 
-  dir_structure_to_string("data").unwrap();
-  //-- MY FIRST
-  let dir_struct_ok = 
-  dir_struct.iter_mut().
-  map(|i|i.replace("./",""))
-  .collect::<Vec<String>>();
+ let run_result = run().unwrap();
+ println!("{:?}",run_result); 
+}//main
 
-  // println!("{:?}",dir_struct_ok); 
-  //---------------------------------
-  for dir in dir_struct_ok{
+fn run()->Result<bool,Error>{
+// Step 01: clone_data_to_site
+clone_data_to_site()?;
+// Step 02: get_dir_struct_clean
+let dir_struct_clean = 
+get_dir_struct_clean()?;
+//---------------------------------
+// Step 03: Loop for each sub-dir
+
+  for dir in dir_struct_clean{
     let d = create_dir_brute(&dir.as_str())
     .unwrap();
     println!("{:?}",d);
@@ -50,14 +47,27 @@ pub fn main() {
   let b = 
   create_file_brute(dest_clean.as_str())
   .unwrap();
-      // let r = write_to_file
-      // (&dest_clean, &content).unwrap();
-      println!("{:?}",b);
+      let r = write_to_file
+      (&dest_clean, &content).unwrap();
+      println!("{:?}",r);
               
       }
   }
-}//main
+  Ok(true)
+}//run
 
-fn create_file(){
-create_file_brute("site/abc.md");  
+fn clone_data_to_site()->Result<Vec<String>,Error>{
+clone_dir_structure("data","site")  
+}
+
+fn get_dir_struct_clean()->Result<Vec<String>,Error>{
+  let mut dir_struct = 
+  dir_structure_to_string("data")?;
+  //-- MY FIRST
+  let dir_struct_clean = 
+  dir_struct.iter_mut().
+  map(|i|i.replace("./",""))
+  .collect::<Vec<String>>();
+
+Ok(dir_struct_clean)
 }
